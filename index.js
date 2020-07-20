6f89bd6b97baae6a034486255ea5381b4955189b
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-07-20 17:50:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,4 @@ document.querySelector("#main").remove();
 
 document.body.innerHTML = '<h1 id="victory"></h1>';
 let newHeader = document.querySelector('h1#victory');
-
-newHeader.className = ''
+newHeader.innerHTML = 'Abdulateef is the champion';
